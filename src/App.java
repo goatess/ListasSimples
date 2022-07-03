61fd4b594d17346c785632805afbd8c838ca712c
goatess <107187023+goatess@users.noreply.github.com>
2022-07-03 17:52:46 +0200

    AllTests

--- src/App.java
+++ src/App.java
@@ -2,190 +2,214 @@ import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        ListInArray arrayItemsList = new ListInArray();
+        ArrayList arrayItemsList = new ArrayList();
         arrayItemsList.stringToArray();
         arrayItemsList.compareStringtoArray();
     }
 }
 
 // A list of items in a String
-class ListInString{
+class SimpleList {
     String item = "";
     String itemWithSpace = "";
-    String list_string = "Java es un lenguaje de programación y una plataforma informática que fue comercializada por primera vez en 1995 por Sun Microsystems";
+    String list = "";
     int position_string = 0;
-    int offset;
-    
-    public String addItem_String(){
-        if (list_string == ""){
-            list_string = item;
-        }else list_string += ", " + item;
-        return list_string;
+    int itemLength;
+
+    public String addItem() {
+        if (list == "") {
+            list = item;
+        } else
+            list += ", " + item;
+        return list;
     }
-    public String divideString(int position){
-        for (int i = position; list_string.charAt(position) != ' ' && i < list_string.length(); i++) {
-            char nextChar = list_string.charAt(i);
-            if (nextChar != ' '){
-                item += nextChar; 
+
+    public int searchItem(String value) {
+        int position_String = getPosition_string(value);
+        itemLength = getItemLength(value);
+        item = list.substring(position_String, itemLength + 1);
+        return position_string;
+    }
+
+    public String searchItem(int position) {
+        item = "";
+        for (int i = position; i < list.length(); i++) {
+            char nextChar = list.charAt(i);
+            if (nextChar != ' ') {
+                item += nextChar;
             } else {
-                itemWithSpace = (item += nextChar); 
+                itemWithSpace = item; 
+                itemWithSpace += nextChar;
                 break;
-            } 
+            }
         }
         return item;
     }
-    public void deleteItem_ByValue_String(String value){
-        int position_String = getPosition_string(value);
-        int offset = getOffset(value);
-        String result = list_string.substring(position_String, offset + 1); 
-        value = result;
-        list_string = list_string.replaceAll(value, "");
-    }
-    public String deletItem_ByPosition_String(int position){
-        divideString(position);
-        item = itemWithSpace;
-        list_string = list_string.replaceAll(item, "");
-        return list_string;
+
+    public void deleteItem_ByValue(String value) {
+        list = list.replaceFirst(value, "");
     }
 
-    //setters and getters os StringList
+    public String deleteItem_ByPosition(int position) {
+        searchItem(position);
+        // item = itemWithSpace;
+        list = list.replaceAll(item, "");
+        return list;
+    }
+
+    // setters and getters os StringList
     public String getList_string() {
-        return list_string;
+        return list;
     }
+
     public void setList_string(String fullString) {
-        this.list_string = fullString;
+        this.list = fullString;
     }
+
     public String getItem() {
         return item;
     }
+
     public void setItem(String item) {
         this.item = item;
     }
+
     public int getPosition_string(String searchedItem) {
-        position_string = list_string.indexOf(searchedItem); 
+        position_string = list.indexOf(searchedItem);
         return position_string;
     }
-    public int getOffset(String searchedItem){
-        offset = searchedItem.length();
-        offset += list_string.indexOf(searchedItem);
-        return offset;
+
+    public int getItemLength(String searchedItem) {
+        itemLength = searchedItem.length();
+        itemLength += list.indexOf(searchedItem);
+        return itemLength;
     }
-    public void setDeleteSubString(String subString) {
-        this.item = subString;
-        deleteItem_ByValue_String(subString);
+
+    public void setDeleteSubString(String item) {
+        this.item = item;
+        deleteItem_ByValue(item);
     }
-      public void setDeleteSubString(int searchPosition) {
-        this.position_string = searchPosition;
-        deletItem_ByPosition_String(searchPosition);
+
+    public void setDeleteSubString(int position) {
+        this.position_string = position;
+        deleteItem_ByPosition(position);
     }
 }
 
 // A list of items in an Array
-class ListInArray {
-    ListInString listInString = new ListInString();
-    String[] list_array = null;
-    String list_string = listInString.getList_string();
-    String itemValue = "informática";
+class ArrayList {
+    SimpleList simpleList = new SimpleList();
+    String[] listArray = null;
+    String list = "";
+    String itemValue = "";
     int itemPosition_string = 0;
-    boolean isEqual = true;
+    boolean stringEqualsArray = true;
     int itemPosition_array = 0;
-    
-    public String [] stringToArray(){
-        if (list_string == ""){
-            list_string = listInString.getList_string();
-        }else list_string = list_string.replaceAll("[\\.\\,\\(\\)]", "");
-        list_array = list_string.split(" "); 
-        return list_array;
+
+    public String[] stringToArray() {
+        if (list == "") {
+            list = simpleList.getList_string();
+        } else
+            list = list.replaceAll("[\\.\\,\\(\\)]", "");
+        listArray = list.split(" ");
+        return listArray;
     }
-    public void SortArray(){
-        Arrays.sort(list_array);
+
+    public void sortArray() {
+        Arrays.sort(listArray);
     }
-    public String arrayToString(){
-        list_string = "";
-        for (int i = 0; i < list_array.length; i++) {
-            if (list_string == ""){
-                list_string = list_array[i];
-            }else list_string += " " + list_array[i];
+
+    public String convertArrayToString() {
+        for (int i = 0; i < listArray.length; i++) {
+            if (list == "") {
+                list = listArray[i];
+            } else
+                list += " " + listArray[i];
         }
-        return list_string; 
+        return list;
     }
-    public String arrayToString_Format(){ 
-    list_string = list_string.replaceAll("[\\.\\\\(\\)]", "");
-    list_string = Arrays.toString(list_array);
-    return list_string;
-}
-    public int searchItemPosition(String searchedItem){
-        for (int i = 0; i < list_array.length; i++) {
-            if (list_array[i].equals(searchedItem)) {
+
+    public String convertArrayToString_Format() {
+        list = list.replaceAll("[\\.\\\\(\\)]", "");
+        list = Arrays.toString(listArray);
+        return list;
+    }
+
+    public int searchItemPosition(String searchedItem) {
+        for (int i = 0; i < listArray.length; i++) {
+            if (listArray[i].equals(searchedItem)) {
                 return i;
             }
         }
-        System.out.println(itemPosition_string);
         return itemPosition_string;
-    } 
-    public String searchItemValue(int position){
-        return itemValue;
-    }
-    public String[] eliminateArrayPosition(String item){
-        int pos = searchItemPosition(item);
-        list_array[pos]= "";
-        arrayToString();
-        stringToArray();
-        return list_array;
-    }
-    public String[] eliminateArrayPosition(int position){
-        list_array[position]= "";
-        arrayToString();
-        stringToArray();
-        return list_array;
     }
 
-    public Boolean compareStringtoArray(){
-        list_string = listInString.list_string;
+    public String searchItemValue(int position) {
+        return itemValue;
+    }
+
+    public String[] eliminatePosition(String item) {
+        int pos = searchItemPosition(item);
+        listArray[pos] = "";
+        convertArrayToString();
         stringToArray();
-        arrayToString();
+        return listArray;
+    }
+
+    public String[] eliminatePosition(int position) {
+        listArray[position] = "";
+        convertArrayToString();
+        stringToArray();
+        return listArray;
+    }
+
+    public Boolean compareStringtoArray() {
+        stringToArray();
         int position = 0;
         String item = "";
-        for (int i = 0; i < list_array.length; i++) {
-           
-            position = listInString.position_string + listInString.item.length();
-            item = listInString.divideString(position);
-            listInString.setItem(item);
-           
-            if(list_array[i].equals(listInString.item)) {
-                isEqual = true; 
-                System.out.println("true " + listInString.item);
-            } 
-            else {
-                isEqual = false;
-                System.out.println("false " + listInString.item);
-            } 
+        String arrayItem = "";
+        for (int i = 0; i < listArray.length; i++) {
+            item = simpleList.searchItem(position);
+            position += item.length() + 1;
+            arrayItem = listArray[i];
+            if (arrayItem.equals(item)) {
+                stringEqualsArray = true;
+            } else {
+                stringEqualsArray = false;
+                break;
+            }
         }
-        return isEqual;   
-    }   
+        return stringEqualsArray;
+    }
 
     // Getters and Setters of ArrayItemsList
     public String[] getAllItemsArray() {
-        return list_array;
+        return listArray;
     }
-    public void setAllItemsArray(String[] arrayList) {
-        this.list_array = arrayList;
+
+    public void setFullArray(String[] arrayList) {
+        this.listArray = arrayList;
     }
-    public String getAllItemsString() {
-        return list_string;
+
+    public String getFullString() {
+        return list;
     }
-    public void setAllItemsString(String allItems) {
-        this.list_string = allItems;
+
+    public void setFullString(String allItems) {
+        this.list = allItems;
     }
-    public void setEliminateArrayPosition (String searchedItem){
+
+    public void setEliminatePosition(String searchedItem) {
+        eliminatePosition(searchedItem);
         this.itemValue = searchedItem;
-        eliminateArrayPosition(searchedItem);
     }
-    public void setEliminateArrayPosition (int arrayPosition){
+
+    public void setEliminatePosition(int arrayPosition) {
+        eliminatePosition(arrayPosition);
         this.itemPosition_array = arrayPosition;
-        eliminateArrayPosition(arrayPosition);
     }
-    public boolean getComparation(){
-        return isEqual;
+
+    public boolean getComparation() {
+        return stringEqualsArray;
     }
 }
--- src/AppTest.java
+++ src/AppTest.java
@@ -4,199 +4,219 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class AppTest {
-    
+
     @Test
-    public void checkSetAllItemsinList_String(){
-        //arrange
+    public void setAllItemsinList_String() {
+        // arrange
         final String expectedString = "Java es un lenguaje de programación y una plataforma informática que fue comercializada por primera vez en 1995 por Sun Microsystems";
-        ListInString stringList = new ListInString();
-        
-        //act
-        String actualString = stringList.getList_string();
-        
-        //assert
+        SimpleList simpleList = new SimpleList();
+
+        // act
+        simpleList.setList_string(
+                "Java es un lenguaje de programación y una plataforma informática que fue comercializada por primera vez en 1995 por Sun Microsystems");
+        String actualString = simpleList.getList_string();
+
+        // assert
         assertEquals(expectedString, actualString);
     }
 
     @Test
-    public void checkAddItemToList_String(){
-        //arrange
+    public void addItemToList_String() {
+        // arrange
         final String expectedString = "Java es un lenguaje de programación, y una plataforma informática";
-        ListInString stringList = new ListInString();
+        SimpleList simpleList = new SimpleList();
 
-        //act
-        stringList.setList_string("Java es un lenguaje de programación");
-        stringList.setItem("y una plataforma informática");
-        String actualString = stringList.addItem_String();
-        
-        //assert
+        // act
+        simpleList.setList_string("Java es un lenguaje de programación");
+        simpleList.setItem("y una plataforma informática");
+        String actualString = simpleList.addItem();
+
+        // assert
         assertEquals(expectedString, actualString);
     }
 
     @Test
-    public void checkTakeItemFromList_ByPosition_String(){
-        //arrange
-        final String expectedString = "Java ";
-        ListInString stringList = new ListInString();
-
-        //act
-        stringList.setList_string("Java es un lenguaje de programación");
-        String actualString = stringList.divideString(0);
-        
-        //assert
-        assertEquals(expectedString, actualString);
-    }
-
-    @Test
-    public void checkSearchItemInList_String(){
-        //arrange
+    public void insertItemGetPosition() {
+        // arrange
         final int expectedPosition = 53;
-        ListInString stringList = new ListInString();
+        SimpleList stringList = new SimpleList();
 
-        //act
-        stringList.setList_string("Java es un lenguaje de programación y una plataforma informática que fue comercializada por primera vez en 1995 por Sun Microsystems");
+        // act
+        stringList.setList_string(
+                "Java es un lenguaje de programación y una plataforma informática que fue comercializada por primera vez en 1995 por Sun Microsystems");
         int actualPosition = stringList.getPosition_string("informática");
 
-        //assert
+        // assert
         assertEquals(expectedPosition, actualPosition);
     }
 
     @Test
-    public void checkDeleteItem_BySearchWord_String(){
-        //arrange
-        final int expectedPosition = -1;
-        ListInString stringList = new ListInString();
+    public void searchItem_ByPosition() {
+        // arrange
+        final String expectedString = "informática";
+        SimpleList stringList = new SimpleList();
 
-        //act
-        stringList.setList_string("Java es un lenguaje de programación y una plataforma informática que fue comercializada por primera vez en 1995 por Sun Microsystems");
+        // act
+        stringList.setList_string("Java es un lenguaje de programación y una plataforma informática");
+        String actualString = stringList.searchItem(53);
+
+        // assert
+        assertEquals(expectedString, actualString);
+    }
+
+    @Test
+    public void deleteItem_ByValue() {
+        // arrange
+        final int expectedPosition = -1;
+        SimpleList stringList = new SimpleList();
+
+        // act
+        stringList.setList_string(
+                "Java es un lenguaje de programación y una plataforma informática que fue comercializada por primera vez en 1995 por Sun Microsystems");
         stringList.setDeleteSubString("informática");
         int actualPosition = stringList.getPosition_string("informática");
 
-        //assert
+        // assert
         assertEquals(expectedPosition, actualPosition);
     }
 
     @Test
-    public void checkDeleteItem_ByPosition_String(){
-        //arrange
+    public void deleteItem_ByPosition_chekPosition() {
+        // arrange
         final int expectedPosition = -1;
-        ListInString stringList = new ListInString();
+        SimpleList simpleList = new SimpleList();
 
-        //act
-        stringList.setList_string("Java es un lenguaje de programación y una plataforma informática que fue comercializada por primera vez en 1995 por Sun Microsystems");
-        stringList.setDeleteSubString(53);
-        int actualPosition = stringList.getPosition_string("informática");
+        // act
+        simpleList.setList_string(
+                "Java es un lenguaje de programación y una plataforma informática que fue comercializada por primera vez en 1995 por Sun Microsystems");
+        simpleList.setDeleteSubString(53);
+        int actualPosition = simpleList.getPosition_string("informática");
 
-        //assert
+        // assert
         assertEquals(expectedPosition, actualPosition);
     }
 
     @Test
-    public void checkStringToArrayConversion(){
-        //arrange
-        final String [] expectedArray = {"Java", "es", "un"};
-        ListInArray arrayItemsList = new ListInArray();
+    public void compareSimpleListToArray() {
+        // arrange
+        final boolean expectedComparation = true;
+        ArrayList arrayList = new ArrayList();
 
-        //act
-        arrayItemsList.setAllItemsString("Java es un ");
+        // act
+        boolean actualComparation = arrayList.getComparation();
+
+        // assert
+        assertEquals(expectedComparation, actualComparation);
+    }
+
+    @Test
+    public void stringToArrayConversion() {
+        // arrange
+        final String[] expectedArray = { "Java", "es", "un" };
+        ArrayList arrayItemsList = new ArrayList();
+
+        // act
+        arrayItemsList.setFullString("Java es un ");
         arrayItemsList.stringToArray();
-        String[]actualArray = arrayItemsList.getAllItemsArray();
+        String[] actualArray = arrayItemsList.getAllItemsArray();
 
-        //assert
+        // assert
         assertArrayEquals(expectedArray, actualArray);
     }
 
     @Test
-    public void checkArraytoStringConversion(){
-        //arrange
+    public void arraytoStringConversion() {
+        // arrange
         final String expectedString = "Java es un";
-        ListInArray arrayItemsList = new ListInArray();
+        ArrayList arrayItemsList = new ArrayList();
 
-        //act
-        String[]testArray = {"Java","es","un"};
-        arrayItemsList.setAllItemsArray(testArray);
-        String testString = arrayItemsList.arrayToString();
-        arrayItemsList.setAllItemsString(testString); 
-        String actualString = arrayItemsList.getAllItemsString();
+        // act
+        String[] testArray = { "Java", "es", "un" };
+        arrayItemsList.setFullArray(testArray);
+        arrayItemsList.convertArrayToString();
+        String actualString = arrayItemsList.getFullString();
 
-        //assert
+        // assert
         assertEquals(expectedString, actualString);
     }
-    
+
     @Test
-    public void checkArrayEqualsToAnotherArray(){
-        //arrange
-        final String [] expectedArray = {"Java", "es", "un"};
-        ListInArray arrayItemsList = new ListInArray();
+    public void arrayEqualsToAnotherArray() {
+        // arrange
+        final String[] expectedArray = { "Java", "es", "un" };
+        ArrayList arrayItemsList = new ArrayList();
 
-        //act
-        arrayItemsList.setAllItemsString("Java es un ");
+        // act
+        arrayItemsList.setFullString("Java es un ");
         arrayItemsList.stringToArray();
-        String[]actualArray = arrayItemsList.getAllItemsArray();
+        String[] actualArray = arrayItemsList.getAllItemsArray();
 
-        //assert
-        assertArrayEquals( expectedArray, actualArray);
+        // assert
+        assertArrayEquals(expectedArray, actualArray);
     }
 
     @Test
-    public void checkIfStringEqualsToArray(){
-        //arrange
+    public void stringEqualsToArray() {
+        // arrange
         final boolean expectedBoolean = true;
-        ListInArray arrayItemsList = new ListInArray();
+        ArrayList arrayItemsList = new ArrayList();
+        SimpleList simpleList = new SimpleList();
 
-        //act
+        // act
+        simpleList.setList_string(
+                "Java es un lenguaje de programación y una plataforma informática que fue comercializada por primera vez en 1995 por Sun Microsystems");
         arrayItemsList.compareStringtoArray();
         boolean actualBoolean = arrayItemsList.getComparation();
-        
-        //assert
+
+        // assert
         assertEquals(expectedBoolean, actualBoolean);
     }
-    
-    @Test
-    public void checkSearchArrayItem_ByPosition(){
-        //arrange
-        final int expectedPosition = 9;
-        ListInArray arrayItemsList = new ListInArray();
 
-        //act
-        arrayItemsList.setAllItemsString("Java es un lenguaje de programación y una plataforma informática que fue comercializada por primera vez en 1995 por Sun Microsystems");
+    @Test
+    public void searchArrayItemPosition() {
+        // arrange
+        final int expectedPosition = 9;
+        ArrayList arrayItemsList = new ArrayList();
+
+        // act
+        arrayItemsList.setFullString(
+                "Java es un lenguaje de programación y una plataforma informática que fue comercializada por primera vez en 1995 por Sun Microsystems");
         arrayItemsList.stringToArray();
         int actualPosition = arrayItemsList.searchItemPosition("informática");
 
-        //assert
+        // assert
         assertEquals(expectedPosition, actualPosition);
     }
-    
-    @Test
-    public void checkEliminateArrayPositionBySearchWord(){
-        //arrange
-        final String[] expectedArray = {"es","un","lenguaje"};
-        ListInArray arrayItemsList = new ListInArray();
 
-        //act
-        arrayItemsList.setAllItemsString("Java es un lenguaje");
-        arrayItemsList.stringToArray();
-        arrayItemsList.setEliminateArrayPosition("Java");
+    @Test
+    public void eliminateArrayPositionByValue() {
+        // arrange
+        String[] testArray = { "Java", "es", "un", "lenguaje" };
+        final String[] expectedArray = { "es", "un", "lenguaje" };
+        ArrayList arrayItemsList = new ArrayList();
+
+        // act
+        arrayItemsList.setFullArray(testArray);
+        arrayItemsList.eliminatePosition("Java");
         String[] actualArray = arrayItemsList.getAllItemsArray();
 
-        //assert
+        // assert
         assertArrayEquals(expectedArray, actualArray);
     }
 
     @Test
-    public void checkEliminateArrayPositionByIndex(){
-        //arrange
-        final String[] expectedArray = {"es","un","lenguaje"};
-        ListInArray arrayItemsList = new ListInArray();
+    public void eliminateArrayPositionByIndex() {
+        // arrange
+        String[] testArray = { "Java", "es", "un", "lenguaje" };
+        final String[] expectedArray = { "es", "un", "lenguaje" };
+        ArrayList arrayItemsList = new ArrayList();
 
-        //act
-        arrayItemsList.setAllItemsString("Java es un lenguaje");
-        arrayItemsList.stringToArray();
-        arrayItemsList.setEliminateArrayPosition(0);
+        // act
+        arrayItemsList.setFullArray(testArray);
+        arrayItemsList.eliminatePosition(0);
         String[] actualArray = arrayItemsList.getAllItemsArray();
 
-        //assert
+        // assert
         assertArrayEquals(expectedArray, actualArray);
     }
-
 }
